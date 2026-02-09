7353e181133bb2009588b2dbf0564acfbf3a8efb
zyntax6760 <kinghojoon12@gmail.com>
2026-02-09 19:44:17 +0900

    새 기능 추가
    - 메시지 검열 기능 추가
    - 메시지 검열 단어 목록 추가

---
+++ text_censorship/text_censorship.js
@@ -0,0 +1,62 @@
+const { EmbedBuilder } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+function getFilter() {
+  try {
+    const data = fs.readFileSync(path.join(__dirname, "filter.json"), "utf8");
+    const json = JSON.parse(data);
+    return json.Notext ? json.Notext : json;
+  } catch (err) {
+    console.error("Failed to read filter list : ", err);
+    return {};
+  }
+}
+
+module.exports = (client) => {
+  client.on("messageCreate", async (message) => {
+    if (message.author.bot || !message.content) return;
+
+    const filter = getFilter();
+    let content = message.content;
+    const cleanText = content.replace(/[\s\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g, "");
+    
+    let isModified = false;
+    let finalContent = content;
+
+    for (const [replacement, badWords] of Object.entries(filter)) {
+      for (const badWord of badWords) {
+        if (content.includes(badWord) || cleanText.includes(badWord)) {
+          const escapeWord = badWord.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+          const regex = new RegExp(escapeWord, "g");
+          
+          finalContent = finalContent.replace(regex, replacement);
+          isModified = true;
+        }
+      }
+    }
+
+    if (isModified) {
+      try {
+        if (message.deletable) {
+          await message.delete();
+        }
+
+        const filterEmbed = new EmbedBuilder()
+          .setColor(0x00FF7F)
+          .setTitle("검열 딱!")
+          .setDescription(finalContent)
+          .setAuthor({ 
+            name: message.author.username, 
+            iconURL: message.author.displayAvatarURL() 
+          })
+          .setTimestamp();
+
+        await message.channel.send({ embeds: [filterEmbed] });
+
+      } catch (err) {
+        console.error("Failed to process message : ", err);
+      }
+    }
+  });
+};
